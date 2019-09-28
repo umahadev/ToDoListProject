86eaf2d3f51b0c342cd486f71cf72f841f4edc0e
umahadev <39602116+umahadev@users.noreply.github.com>
2019-09-28 18:59:44 -0400

    Checkbox is added

--- script.js
+++ script.js
@@ -9,21 +9,43 @@ addDataBtn.onclick = function(event) {
 
 //function to read the data from input text box
 function getToDoData(){
-    var toDoText = document.getElementById('todo-text').value;
-    console.log(toDoText);
-    addToDoListTable(toDoText)
+    var str = document.getElementById('todo-text').value;
+    
+    if( str.length != 0){
+    console.log("text val="+ str + str.length);
+    addToDoListTable(str)
+    }
+    else{
+        console.log("text is null");
+    }
 
 }
 
+//function to add to the List Table
 function addToDoListTable(toDoText){
+    // Create a table row to append
     row=document.createElement("tr");
+    // create two cells/col - one to display the checkbox and otherone to display the text
     cell1 = document.createElement("td");
+    cell2 = document.createElement("td");
+    //create a checkbox to insert in cell1
+     // creating checkbox element 
+    var checkbox = document.createElement('input'); 
+    // Assigning the attributes 
+    // to created checkbox 
+    
+    checkbox.type = "checkbox"; 
+    //Create a textnode to insert in cell2
     var newToDoList = document.createTextNode(toDoText);
-    cell1.appendChild(newToDoList);
-    row.appendChild(cell1);
-    toDoListTable.appendChild(row);
+    cell2.appendChild(newToDoList);
+    row.appendChild(checkbox);
+    row.appendChild(cell2)
 
+    toDoListTable.appendChild(row);
+     
 }
+
+
 
 
 //after that build a dynamic table and update the todo list without deleting the old one
