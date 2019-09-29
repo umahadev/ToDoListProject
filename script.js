8f4bf508cc3eff4ba1a18dc296f0dd6fc1324f6d
umahadev <39602116+umahadev@users.noreply.github.com>
2019-09-29 19:12:51 -0400

    Remove button is added

--- script.js
+++ script.js
@@ -25,25 +25,43 @@ function getToDoData(){
 function addToDoListTable(toDoText){
     // Create a table row to append
     row=document.createElement("tr");
-    // create two cells/col - one to display the checkbox and otherone to display the text
-    cell1 = document.createElement("td");
+    // create three cells/col - one to display the checkbox, 2nd to display the text and 3rd to display the remove button
+    //cell1 = document.createElement("td");
     cell2 = document.createElement("td");
+    cell3 = document.createElement("td");
     //create a checkbox to insert in cell1
      // creating checkbox element 
     var checkbox = document.createElement('input'); 
     // Assigning the attributes 
     // to created checkbox 
-    
     checkbox.type = "checkbox"; 
+    row.appendChild(checkbox);
+    
+
     //Create a textnode to insert in cell2
     var newToDoList = document.createTextNode(toDoText);
     cell2.appendChild(newToDoList);
-    row.appendChild(checkbox);
-    row.appendChild(cell2)
+    row.appendChild(cell2);
 
-    toDoListTable.appendChild(row);
+    //add the remove button
+    
+    var btnRemove= document.createElement('input');
+    btnRemove.setAttribute('type','button');
+    btnRemove.setAttribute('name','Remove');
+    btnRemove.setAttribute('value','Remove');
+    btnRemove.setAttribute('onclick', 'removeRow(this)');
+    cell3.appendChild(btnRemove);
+    row.appendChild(cell3);
+     toDoListTable.appendChild(row);
      
 }
+
+ // DELETE TABLE ROW.
+ function removeRow(oButton) {
+    var empTab = document.getElementById('todo-list-table');
+    empTab.deleteRow(oButton.parentNode.parentNode.rowIndex);       // BUTTON -> TD -> TR.
+}
+
 
 
 
